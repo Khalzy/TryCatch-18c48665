2fe49377736f54b8d8cfae93179a83760bda3861
Candace <2100978@talnet.nl>
2020-02-27 11:51:08 +0100

    Uitwerking van de exercise

---
+++ catch.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+function  countDown()
+{
+    $x = rand(1, 20);
+    try {
+        if ($x >= 0 && $x <= 10) {
+            throw new Exception("Er is iets fouts gegaan! ");
+        } else {
+            echo $x;
+        }
+    } catch (Exception $e) {
+
+        echo "Error: ", $e->getMessage();
+    }
+}
+
+countDown();
